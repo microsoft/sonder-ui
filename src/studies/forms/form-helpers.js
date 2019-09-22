f04d29fcbed3d88a56724e5d0a366690ed76991b
Sarah Higley <sarah.higley@microsoft.com>
2019-09-21 23:30:11 -0700

    move all form logic to form-helpers

--- src/studies/forms/form-helpers.js
+++ src/studies/forms/form-helpers.js
@@ -3,11 +3,65 @@
 *  Licensed under the MIT license. See License.txt in the project root for license information.
 *--------------------------------------------------------------------------------------------*/
 
+// Easily instantiate a form
+// assumptions:
+// fields have .form-input class
+// errors have a .error class
+// submit button has a .submit class
+// form-level notification region has .form-alert class
+// all other form content has .form-content class
+var UsabilityForm = function(el) {
+  this.form = el;
+  this.fields = Array.prototype.slice.call(document.querySelectorAll('.form-input'));
+  this.submitButton = document.querySelector('.submit');
+  this.fieldValidations = this.fields.map((field) => false);
+  this.formNotificationEl = el.querySelector('.form-alert');
+}
+
+// attach listeners
+UsabilityForm.prototype.init = function() {
+  var _this = this;
+  this.fields.forEach(function (field) {
+    field.addEventListener('blur', function(event) {
+      var index = _this.fields.indexOf(event.target);
+      var isValid = this.validateField(event.target);
+      _this.fieldValidations[index] = isValid;
+      _this.checkForm();
+    });
+  });
+
+  this.form.addEventListener('submit', function(event) {
+    event.preventDefault();
+    _this.form.querySelector('.form-content').innerHTML = '';
+    _this.formNotificationEl.innerHTML = 'Success!';
+  });
+}
+
+// check if all field validations are currently true
+UsabilityForm.prototype.checkForm = function() {
+  var isFormValid = true;
+  this.fieldValidations.forEach(function(valid) {
+    if (!valid) {
+      isFormValid = false;
+      return;
+    }
+  });
+
+  if (isFormValid) {
+    this.submitButton.removeAttribute('disabled');
+  }
+  else {
+    this.submitButton.setAttribute('disabled', true);
+  }
+
+  return isFormValid;
+}
+
 // validateField function: accepts HTMLInputElement | HTMLSelectElement, returns boolean valid/not valid
 // assumes all fields are at least required
 // check for a data-expected string; if present, perform regexp check against that
 // check for a data-errormessage string; if present and failed data-expected, display custom error message
-function validateField(field) {
+UsabilityForm.prototype.validateField = function(field) {
   const value = field.value.trim();
   const regexString = field.getAttribute('data-expected');
   const errorEl = field.parentNode.querySelector('.error');
@@ -20,7 +74,7 @@ function validateField(field) {
     errormessage = 'This field is required';
     valid = false;
   }
-  else if (regexString && !value.match(new RegExp(regexString)).length === 1) {
+  else if (regexString && !value.match(new RegExp(regexString))) {
     errormessage = field.getAttribute('data-errormessage') || 'Please enter a valid value';
     valid = false;
   }
